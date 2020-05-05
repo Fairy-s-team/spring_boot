e4bca0373402e40aa1cc98e34f5595609db8185a
wwj <3053347069@qq.com>
2020-05-05 15:41:22 +0800

    first commit

---
+++ src/test/java/com/teaching/TeachingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
